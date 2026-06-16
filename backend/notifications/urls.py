e9367d726f796a18b7ee5c1e0aeafcbf0a3e876c
Ashray Sikka <ashray15.sikka@gmail.com>
2026-06-16 17:27:56 -0400

    chore: remove temporary recalculate endpoint

--- backend/notifications/urls.py
+++ backend/notifications/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from notifications.views import SendTestNotificationView, CronSendRemindersView, RecalculateEntriesView
+from notifications.views import SendTestNotificationView, CronSendRemindersView
 
 urlpatterns = [
     path('send-test/', SendTestNotificationView.as_view(), name='send-test'),
     path('cron/', CronSendRemindersView.as_view(), name='cron-reminders'),
-    path('recalculate/', RecalculateEntriesView.as_view(), name='recalculate'),
 ]
